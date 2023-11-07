f818e7d8eff8c1caeaa09f7e03efa87a9e0f9c1b
Adam Byerly <abyerly2@jhu.edu>
2023-11-06 19:32:08 -0500

    Updated scenario type syntax

--- src/lib.mli
+++ src/lib.mli
@@ -11,22 +11,28 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Core
-
 (* Player *)
-type Player
+type player
 
 (* Game Scenario *)
-type ActionCard
-type InvestmentCard
-type DomesticEventCard
-type InternationalEventCard
-type StartingCondition
-type VictoryCondition
+type action_card
+type action_card_deck = action_card list
 
-type Scenario = {
-    player_decks: (Player * (ActionCard list * InvestmentCard list * DomesticEventCard list)) list;
-    international_events: InternationalEventCard list;
-    starting_condition: StartingCondition;
-    victory_condition: VictoryCondition;
+type investment_card
+type investment_card_deck = investment_card list
+
+type domestic_event_card
+type domestic_event_card_deck = domestic_event_card list
+
+type international_event_card
+type international_event_card_deck = international_event_card list
+
+type starting_condition
+type victory_condition
+
+type scenario = {
+    player_decks: (player * (action_card_deck * international_event_card_deck * domestic_event_card_deck)) list;
+    international_events: international_event_card_deck;
+    starting_condition: starting_condition;
+    victory_condition: victory_condition;
 }
