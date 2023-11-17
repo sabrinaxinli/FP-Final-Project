046c1d5e11660eaf058cdac666f07db5c863a1d5
Adam Byerly <abyerly2@jhu.edu>
2023-11-17 16:02:25 -0500

    WIP

--- src/game.mli
+++
@@ -1,7 +0,0 @@
-module type VictoryCondition : sig
-    type Player
-    type IP
-    val check_for_victory : (Player * IP) List -> (Player * Bool) List
-end
-
-module Create_victory_condition : VictoryCondition
